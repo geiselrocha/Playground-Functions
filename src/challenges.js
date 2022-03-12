91290b55bbbf3c6dfbe6d441e2ecc15827f29068
geiselrocha <geiselrocha@hotmail.com>
2022-03-12 12:41:32 -0300

    Desafio 1

--- src/challenges.js
+++ src/challenges.js
@@ -1,7 +1,11 @@
 // Desafio 1
-function compareTrue() {
-  // add
+function compareTrue(value1, value2) {
+  if (value1 === true && value2 === true) {
+    return true;
+  }
+  return false;
 }
+console.log(compareTrue());
 
 // Desafio 2
 function calcArea() {
